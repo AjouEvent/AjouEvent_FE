030fec51c2c0442662c9b623fbec5591a4235a09
mkms8436 <mkms8436@gmail.com>
2026-03-28 17:48:23 +0900

    fix: BE에서 사용 안하는 endpoint 삭제(#115)

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -12,26 +12,5 @@ export const deleteUser = () =>
 export const oauthLogin = (loginData) =>
   axios.post(`${BASE_URL}/api/users/oauth`, loginData, { withCredentials: true });
 
-export const login = (userData) =>
-  axios.post(`${BASE_URL}/api/users/login`, userData);
-
-export const register = (userData) =>
-  axios.post(`${BASE_URL}/api/users/register`, userData);
-
 export const registerInfo = (data) =>
   api.post('/api/users/register-info', data);
-
-export const checkDuplicateEmail = (email) =>
-  axios.get(`${BASE_URL}/api/users/duplicateEmail?email=${email}`);
-
-export const checkAccountExists = (email, name) =>
-  axios.get(`${BASE_URL}/api/users/accountExists?email=${email}&name=${name}`);
-
-export const requestEmailVerification = (email) =>
-  axios.post(`${BASE_URL}/api/users/emailCheckRequest?email=${email}`);
-
-export const verifyEmailCode = (email, code) =>
-  axios.post(`${BASE_URL}/api/users/emailCheck?email=${email}&code=${code}`);
-
-export const resetPassword = (email, newPassword) =>
-  axios.patch(`${BASE_URL}/api/users/reset-password`, { email, newPassword });
